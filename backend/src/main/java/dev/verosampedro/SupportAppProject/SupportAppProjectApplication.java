6022a140c3598d32ea43bd9f2746e1bcd1e02d11
Vero-Sampedro <verosampedro@icloud.com>
2023-12-19 14:47:08 +0100

    inializated repository

---
+++ backend/src/main/java/dev/verosampedro/SupportAppProject/SupportAppProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.verosampedro.SupportAppProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAppProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/verosampedro/SupportAppProject/SupportAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.verosampedro.SupportAppProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
