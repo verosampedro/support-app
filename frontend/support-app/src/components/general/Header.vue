c691f2ae796b55c53b3bf17ebb451ac9bf5ca915
Vero-Sampedro <verosampedro@icloud.com>
2023-12-19 15:04:14 +0100

    organize files and folders

---
+++ frontend/support-app/src/components/general/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+
+
+    <img alt="" src="@/assets/logo.svg"/>
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+
+
+</template>
+
+<style scoped>
+
+header {
+    background-color: white;
+}
+</style>
--- frontend/support-app/src/views/HomeView.vue
+++ frontend/support-app/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>This is the home page</p>
   </main>
 </template>
