b1d92b18bbe27f6011fd1d32c461dc071b6efcf4
Vero-Sampedro <verosampedro@icloud.com>
2023-12-20 11:58:58 +0100

    header for mobile and desktop done

--- frontend/support-app/src/components/general/Header.vue
+++ frontend/support-app/src/components/general/Header.vue
@@ -5,18 +5,60 @@
 <template>
 
 
-    <div class="wrapper">
-      <img src="/assets/images/logo.png" alt="">
+    <header class="wrapper">
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <div id="headerContainer">
+
+        <img src="/assets/images/logo.png" alt="">
+
+        <RouterLink class="routerLink" to="/">LOG OUT</RouterLink>
+
+      </div>
+
+    </header>
 
 
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan&family=Neucha&display=swap');
+
+#headerContainer {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .routerLink {
+    text-decoration: none;
+    color: white;
+    padding: 10px;
+
+    font-family: 'League Spartan', sans-serif;
+    align-content: center;
+  }
+
+  img {
+    width: 20%;
+    padding: 10px;
+  }
+}
+
+@media only screen and (min-width: 768px) {
+
+  #headerContainer {
+
+    img {
+      width: 6%;
+      margin-left: 10px;
+    }
+
+    .routerLink {
+      margin-right: 15px;
+      font-size: 1.3rem;
+    }
+  }
+}
 
 </style>
