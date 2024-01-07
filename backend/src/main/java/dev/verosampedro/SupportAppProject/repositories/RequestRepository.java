d25c66fc9306969dccdd9dc199e6eeb3ff86be56
Vero-Sampedro <verosampedro@icloud.com>
2024-01-07 12:13:19 +0100

    changes

--- backend/src/main/java/dev/verosampedro/SupportAppProject/repositories/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.verosampedro.SupportAppProject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import dev.verosampedro.SupportAppProject.models.Request;
-
-public interface RequestRepository extends JpaRepository<Request, Long> {
-}
